8d2d511524208f7472b8e127430a3817e0bdd439
jhb563 <jhbowen047@gmail.com>
2016-06-17 18:43:57 -0700

    Added response.

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res) {
 });
 
 app.post('/message', function(req, res) {
+  var twiml = new twilioClient.TwimlResponse();
+  twiml.message('Thanks for the text');
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
   console.log("Received Request");
 });
 
