c764c316b7e2449e70a5f6862ebe1ef50443463f
jhb563 <jhbowen047@gmail.com>
2016-06-17 19:00:13 -0700

    Logging whole req.

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', function(req, res) {
 
 app.post('/message', function(req, res) {
   var twiml = new twilio.TwimlResponse();
-  console.log(req.query.body);
+  console.log(req);
   if (req.query.body == 'start') {
     twiml.message("Game started!");
   } else {
