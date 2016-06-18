39523d3e03f23f303be7094364a13c4444cb05e9
jhb563 <jhbowen047@gmail.com>
2016-06-17 18:38:48 -0700

    Listen for twill messages.

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var twilio = require('twilio');
-var creds = require('./creds.js');
+//var creds = require('./creds.js');
 
 var app = express();
+//var twilioClient = new twilio.RestClient(creds.sid, creds.token);
 
 app.get('/', function(req, res) {
     res.send("Hello");
+});
+
+app.post('/message', function(req, res) {
+  console.log("Received Request");
 });
 
 var port = process.env.PORT || 5000;
