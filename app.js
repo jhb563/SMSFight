517c96d3105f11b59f267ad1faf07fdf458389a4
jhb563 <jhbowen047@gmail.com>
2016-06-17 20:22:04 -0700

    Saving game.

--- app.js
+++ app.js
@@ -14,14 +14,41 @@ app.get('/', function(req, res) {
     res.send("Hello");
 });
 
+var STARTING_HEALTH = 20;
+
 app.post('/message', function(req, res) {
   var twiml = new twilio.TwimlResponse();
   var fromNumber = req.body.From;
+  var message = req.body.Body;
   Game.Game.findOne({phoneNumber: fromNumber, finished: true}).exec(function(err, game) {
     if(err) throw err;
-    if (!game) {
-      console.log("Should start new game!");
-    }
+    if (!game && message == 'start') {
+      var c1 = new Character.Character({
+        health: STARTING_HEALTH
+      });
+      var c2 = new Character.Character({
+        health: STARTING_HEALTH
+      });
+      var newGame = new Game.Game({
+        player1 = c1,
+        player2 = c2,
+        phoneNumber = fromNumber
+      });
+      newGame.save();
+      // Start the game by either asking the player for their first move
+      // or making a move for the computer and THEN asking the player for
+      // their move. 
+      twiml.message('You have started a new game!');
+    } else if (game) {
+      // interpret then move! 
+      console.log(game.player1.health);
+      console.log(game.player2.health);
+      console.log(game.phoneNumber);
+      twiml.message('You have started a new game');
+    } else {
+      twiml.message('I do not understand your message!');
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
   });
   /*
   if (req.body.Body == 'start') {
